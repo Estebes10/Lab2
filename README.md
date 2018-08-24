448f86769f2213f6b4eb9e0de615e964f937d048
Juan Carlos Estebes <jhon10njc@gmail.com>
2018-08-23 20:11:31 -0500

    Lab2 - Activities

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # LabsMoviles
+# Lab2
--- app/src/main/java/dev/estebes10/com/labs/MainActivity.java
+++ app/src/main/java/dev/estebes10/com/labs/MainActivity.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnMain;
+    private Button btnLogin, btnSettings, btnInvited;
+    private TextView txtMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +18,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        btnMain = (Button)findViewById(R.id.btnMain);
+        txtMain = (TextView)findViewById(R.id.txtMain);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnSettings = (Button)findViewById(R.id.btnSettings);
+        btnInvited = (Button)findViewById(R.id.btnInvited);
 
-        btnMain.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                txtMain.setText("Login works");
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                intent.putExtra("LOGIN", "Login was clicked");
                 startActivity(intent);
-                finish();
+            }
+        });
+
+        btnInvited.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtMain.setText("Invited button was clicked!");
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                intent.putExtra("INVITED", "Invited button was clicked");
+                startActivity(intent);
+            }
+        });
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtMain.setText("Settings was clicked!");
+                Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                intent.putExtra("SETTINGS", "Settings button was clicked");
+                startActivity(intent);
             }
         });
     }
